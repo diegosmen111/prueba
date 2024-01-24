0b1590282545061ae6b6dc270f225f9c4023bb8e
Diego Alvarado <87030684+diegosmen111@users.noreply.github.com>
2024-01-24 09:14:00 +0000

    update : fix codigo

---
+++ tu-proyecto/src/pages/errors/Error.js
@@ -0,0 +1,10 @@
+function Error() {
+    return (
+        <div className="h-full w-full flex items-center justify-center">
+            <h1 className="text-4xl text-red-500 text-center">
+                Oops. Didn&apos;t catch Pokemon.
+            </h1>
+        </div>
+    );
+}
+export default Error;
